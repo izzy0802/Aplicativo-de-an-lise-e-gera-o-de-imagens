b46ff6a0331f94f7ec5d7d5cfc5d6f8975a2c84f
Isis Tristao dos Santos <isis.santos@studentambassadors.com>
2023-10-31 10:41:12 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React, { useState } from 'react';
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState(''); // Estado para armazenar a URL da imagem
+
+  const handleImageAnalysis = () => {
+    // Lógica para análise de imagem
+    console.log('Analisando imagem:', imageUrl);
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para geração de imagem
+    console.log('Gerando imagem com prompt:', imageUrl);
+  };
+
+  return (
+    <div className="App">
+      <h1>Título da Aplicação</h1>
+
+      <input 
+        type="text" 
+        placeholder="Insira a URL da imagem ou o prompt" 
+        value={imageUrl} 
+        onChange={(e) => setImageUrl(e.target.value)} 
+      />
+
+      <button onClick={handleImageAnalysis}>Analisar Imagem</button>
+      <button onClick={handleImageGeneration}>Gerar Imagem</button>
+    </div>
+  );
 }
 
 export default App;
